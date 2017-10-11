6beb10cdcb73de2b4d91b4d9bb2c407748645552
baldore <acornssn@gmail.com>
2017-10-11 10:49:47 -0500

    Improve modules-and-functions-5 with guards

--- exercises/chapter-6/modules-and-functions-5.exs
+++ exercises/chapter-6/modules-and-functions-5.exs
@@ -1,4 +1,4 @@
 defmodule GCD do
-  def gcd(x, 0), do: x
-  def gcd(x, y), do: gcd(y, rem(x, y))
+  def gcd(x, 0) when x >= 0, do: x
+  def gcd(x, y) when x >= 0 and y >= 0, do: gcd(y, rem(x, y))
 end
