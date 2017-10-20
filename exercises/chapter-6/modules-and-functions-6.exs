99b6d036b8b2c192d0f97382a9b01d70edaa54f6
baldore <acornssn@gmail.com>
2017-10-20 15:03:17 -0500

    Improve modules-and-functions-6 with styleguide recommendations

--- exercises/chapter-6/modules-and-functions-6.exs
+++ exercises/chapter-6/modules-and-functions-6.exs
@@ -1,26 +1,26 @@
 defmodule Chop do
   def guess(number_to_guess, range) do
-    _guess(number_to_guess, range, halfway(range))
+    do_guess(number_to_guess, range, halfway(range))
   end
 
-  def _guess(number_to_guess, _, guessed) when number_to_guess == guessed do
+  def do_guess(number_to_guess, _, guessed) when number_to_guess == guessed do
     IO.puts "Is it #{guessed}"
 
     number_to_guess
   end
 
-  def _guess(number_to_guess, l.._, guessed) when number_to_guess < guessed do
+  def do_guess(number_to_guess, l.._, guessed) when number_to_guess < guessed do
     IO.puts "Is it #{guessed}"
 
     new_range = l..(guessed - 1)
-    _guess(number_to_guess, new_range, halfway(new_range))
+    do_guess(number_to_guess, new_range, halfway(new_range))
   end
 
-  def _guess(number_to_guess, _..h, guessed) when number_to_guess > guessed do
+  def do_guess(number_to_guess, _..h, guessed) when number_to_guess > guessed do
     IO.puts "Is it #{guessed}"
 
     new_range = (guessed + 1)..h
-    _guess(number_to_guess, new_range, halfway(new_range))
+    do_guess(number_to_guess, new_range, halfway(new_range))
   end
 
   def halfway(l..h) do
