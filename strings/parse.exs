3e16b3596689a59e404559bfaf4480358a653b22
baldore <acornssn@gmail.com>
2017-11-15 09:56:59 -0500

    Improve strings/parse.exs

--- strings/parse.exs
+++ strings/parse.exs
@@ -8,11 +8,8 @@ defmodule Parse do
 
   defp _number_digits([], value),
     do: value
-  defp _number_digits([ digit | tail ], value)
-  when digit in '0123456789' do
-    _number_digits(tail, value*10 + digit - ?0)
-  end
-  defp _number_digits([ non_digit | _ ], _) do
-    raise "Invalid digit '#{[non_digit]}'"
-  end
+  defp _number_digits([ digit | tail ], value) when digit in '0123456789',
+    do: _number_digits(tail, value*10 + digit - ?0)
+  defp _number_digits([ non_digit | _ ], _),
+    do: raise "Invalid digit '#{[non_digit]}'"
 end
